5148f7cce9ca990905850084e603e9b1c07cd209
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-06 17:01:14 +0100

    Test fail: give as result the same number

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,1 +1,6 @@
-export class Calculator {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export class Calculator {
+  add (_numbers: string): number {
+    return -1
+  }
+}
--- src/__tests__/Calculator.ts
+++ src/__tests__/Calculator.ts
@@ -1,1 +1,18 @@
+import { Calculator } from '../Calculator'
 
+/**
+ * TODO: Test cases
+ * * add('1') -> 1
+ * * add('5') -> 5
+ * * add('10') -> 10
+ */
+
+describe('The kata String Calculator should', () => {
+  it('give as result the same number', () => {
+    const calculator = new Calculator()
+
+    expect(calculator.add('1')).toBe(1)
+    expect(calculator.add('5')).toBe(5)
+    expect(calculator.add('10')).toBe(10)
+  })
+})
