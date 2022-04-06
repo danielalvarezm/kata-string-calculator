38ff5ca02bb6ed469e28905e971ea766e9d4ca8d
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-06 18:13:33 +0100

    Test fail: give the sum of two numbers delimited by comma

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 export class Calculator {
   add (numbers: string): number {
     if (numbers === '') return 0
--- src/__tests__/Calculator.ts
+++ src/__tests__/Calculator.ts
@@ -7,17 +7,21 @@ import { Calculator } from '../Calculator'
  */
 
 describe('The kata String Calculator should', () => {
-  it('give as result the same number', () => {
-    const calculator = new Calculator()
+  const calculator = new Calculator()
 
+  it('give as result the same number', () => {
     expect(calculator.add('1')).toBe(1)
     expect(calculator.add('5')).toBe(5)
     expect(calculator.add('10')).toBe(10)
   })
 
   it('give zero if receive an empty string', () => {
-    const calculator = new Calculator()
-
     expect(calculator.add('')).toBe(0)
+  })
+
+  it('give the sum of two numbers delimited by comma', () => {
+    expect(calculator.add('1,2')).toBe(3)
+    expect(calculator.add('5,10')).toBe(15)
+    expect(calculator.add('10,5')).toBe(15)
   })
 })
