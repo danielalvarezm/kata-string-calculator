7e44971de333fdd995b639036d92bc16c10fdf9b
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-07 21:05:54 +0100

    Adding documentation

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,9 +1,11 @@
-/**
- * TODO: Añadir documentación al código
- */
 export class StringCalculator {
   private delimiters: RegExp = /[,\n]/
 
+  /**
+   * This method processes the input string and calls the add method
+   * @param input Input to be processed
+   * @returns The add method result
+   */
   public processInput (input: string): number {
     if (input === '') return 0
     if (input.startsWith('//')) {
@@ -14,10 +16,14 @@ export class StringCalculator {
     return this.add(input)
   }
 
+  /**
+   * Recursive method that sums the numbers in the input string
+   * @param input Input that contains the numbers to be added
+   * @returns The sum of the numbers in the input string
+   */
   private add (input: string): number {
     const numbers = input.split(this.delimiters)
-    if (this.clearInvalidCases(numbers[0])) numbers[0] = '0'
-    if (isNaN(parseInt(numbers[0]))) throw new Error('invalid syntax')
+    if (this.checkInvalidCases(numbers[0])) numbers[0] = '0'
 
     // base case
     if (numbers.length <= 1) return parseInt(numbers[0])
@@ -26,36 +32,61 @@ export class StringCalculator {
     return parseInt(first) + this.add(rest.join(','))
   }
 
+  /**
+   * This method checks if there are negative numbers in the input string
+   * @param numbers String that contains the numbers to be checked
+   * @returns If there are negative numbers in the input string
+   */
   private checkNegatives (numbers: string): boolean {
     if (!numbers.includes('-')) return false
+
     if (numbers[0] === '-') return true
     else if (numbers.includes('-') && this.delimiters.source.indexOf('-') === -1) return true
-    else if (numbers.includes('-') && numbers.match(this.regexForNegatives())) return true
+    else if (numbers.includes('-') && numbers.match(this.regexForMinusDelimiter())) return true
     return false
   }
 
-  private clearInvalidCases (number: string): boolean {
+  /**
+   * This method ignores specific cases and returns true if the input is invalid
+   * @param number String to be checked
+   * @returns If the input is invalid
+   */
+  private checkInvalidCases (number: string): boolean {
     if (parseInt(number) > 1000 || number.match(/[a-zA-Z]/)) return true
+    if (isNaN(parseInt(number))) throw new Error('invalid syntax')
     return false
   }
 
+  /**
+   * This method updates the delimiters attribute
+   * @param input Input that contains the delimiters
+   */
   private updateDelimiters (input: string): void {
     if (input.length === 1) {
-      this.delimiters = new RegExp(`[,]|[${this.escapeRegExp(input)}]`)
+      this.delimiters = new RegExp(`[,]|[\\n]|[${this.escapeRegExp(input)}]`)
       return
     }
 
     const delimiters = input.replaceAll('[', ' ').replaceAll(']', '')
     const delimitersRegExp = delimiters.replace(/([^\s]{1})/g, '[$1]')
-    const aux = '[,]|[\\n]' + this.escapeRegExp(delimitersRegExp).replaceAll(' ', '|')
-    this.delimiters = new RegExp(aux)
+    const escapedDelimiters = '[,]|[\\n]' + this.escapeRegExp(delimitersRegExp).replaceAll(' ', '|')
+    this.delimiters = new RegExp(escapedDelimiters)
   }
 
-  private regexForNegatives (): RegExp {
-    const negativeDelimiterLength = this.delimiters.source.match(/[-]/g)!.length + 1
-    return new RegExp(`-${'{' + (negativeDelimiterLength) + '}'}`, 'g')
+  /**
+   * This method returns a regex that matches the '-' delimiter
+   * @returns The regex expression
+   */
+  private regexForMinusDelimiter (): RegExp {
+    const minusDelimiterLength = this.delimiters.source.match(/[-]/g)!.length + 1
+    return new RegExp(`-${'{' + (minusDelimiterLength) + '}'}`, 'g')
   }
 
+  /**
+   * This method escapes special characters in a string
+   * @param delimiters Delimiters to be escaped
+   * @returns Delimiters with special characters escaped
+   */
   private escapeRegExp (delimiters: string): string {
     return delimiters.replace(/[.*+\-?^${}()|\\]/g, '\\$&')
   }
