e1e64e07222cb229ffadc3ec4d61349129182579
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-07 20:45:02 +0100

    Test fail: not accept invalid syntax

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,11 +1,13 @@
+/**
+ * TODO: Añadir documentación al código
+ */
 export class StringCalculator {
   private delimiters: RegExp = /[,\n]/
-  private negativeDelimiterLength: number = 0
 
   public processInput (input: string): number {
     if (input === '') return 0
     if (input.startsWith('//')) {
-      this.updateDelimiters(input.replace('//', ''))
+      this.updateDelimiters(input.replace('//', '').split('\n')[0])
       input = input.split('\n')[1]
     }
     if (this.checkNegatives(input)) throw new Error('negatives not allowed')
@@ -23,29 +25,34 @@ export class StringCalculator {
     return parseInt(first) + this.add(rest.join(','))
   }
 
-  private clearInvalidCases (number: string): boolean {
-    if (parseInt(number) > 1000 || number === '' || number.match(/[a-zA-Z]/)) return true
+  private checkNegatives (numbers: string): boolean {
+    if (!numbers.includes('-')) return false
+    if (numbers[0] === '-') return true
+    else if (numbers.includes('-') && this.delimiters.source.indexOf('-') === -1) return true
+    else if (numbers.includes('-') && numbers.match(this.regexForNegatives())) return true
     return false
   }
 
-  private checkNegatives (numbers: string): boolean {
-    if (!numbers.includes('-')) return false
-
-    const regexForNegatives = new RegExp(`-${'{' + (this.negativeDelimiterLength + 1) + '}'}`, 'g')
-    if (numbers[0] === '-') return true
-    else if (numbers.includes('-') && this.delimiters.source.indexOf('-') === -1) return true
-    else if (numbers.includes('-') && numbers.match(regexForNegatives)) return true // Check for negatives in delimiters
+  private clearInvalidCases (number: string): boolean {
+    if (parseInt(number) > 1000 || number.match(/[a-zA-Z]/)) return true
     return false
   }
 
   private updateDelimiters (input: string): void {
-    const delimiters = input.split('\n')[0]
-    if (delimiters.includes('-')) {
-      this.negativeDelimiterLength = delimiters.lastIndexOf('-') - delimiters.indexOf('-') + 1
+    if (input.length === 1) {
+      this.delimiters = new RegExp(`[,]|[${this.escapeRegExp(input)}]`)
+      return
     }
-    const escapedDelimiters = this.escapeRegExp(delimiters)
-    const regex = new RegExp(`[,${escapedDelimiters.replaceAll('[', '|').replaceAll(']', '')}]`, 'g')
-    this.delimiters = regex
+
+    const delimiters = input.replaceAll('[', ' ').replaceAll(']', '')
+    const delimitersRegExp = delimiters.replace(/([^\s]{1})/g, '[$1]')
+    const aux = '[,]|[\\n]' + this.escapeRegExp(delimitersRegExp).replaceAll(' ', '|')
+    this.delimiters = new RegExp(aux)
+  }
+
+  private regexForNegatives (): RegExp {
+    const negativeDelimiterLength = this.delimiters.source.match(/[-]/g)!.length + 1
+    return new RegExp(`-${'{' + (negativeDelimiterLength) + '}'}`, 'g')
   }
 
   private escapeRegExp (delimiters: string): string {
--- src/__tests__/StringCalculator.ts
+++ src/__tests__/StringCalculator.ts
@@ -1,81 +1,78 @@
 import { StringCalculator } from '../StringCalculator'
 
-/**
- * TODO: Test cases
- * * processInput('1') -> 1
- * * processInput('') -> 0
- * * processInput('1,2') -> 3
- * * processInput('10,5\n2') -> 17
- * * processInput('//;\n10;5;2') -> 17
- */
-
 describe('The kata String Calculator should', () => {
-  let calculator: StringCalculator
+  let stringCalculator: StringCalculator
 
   beforeEach(() => {
-    calculator = new StringCalculator()
+    stringCalculator = new StringCalculator()
   })
 
   it('give as result the same number', () => {
-    expect(calculator.processInput('1')).toBe(1)
-    expect(calculator.processInput('5')).toBe(5)
-    expect(calculator.processInput('10')).toBe(10)
+    expect(stringCalculator.processInput('1')).toBe(1)
+    expect(stringCalculator.processInput('5')).toBe(5)
+    expect(stringCalculator.processInput('10')).toBe(10)
   })
 
   it('give zero if receive an empty string', () => {
-    expect(calculator.processInput('')).toBe(0)
+    expect(stringCalculator.processInput('')).toBe(0)
   })
 
   it('give the sum of two or more numbers delimited by comma', () => {
-    expect(calculator.processInput('1,2')).toBe(3)
-    expect(calculator.processInput('5,10')).toBe(15)
-    expect(calculator.processInput('10,5,2')).toBe(17)
+    expect(stringCalculator.processInput('1,2')).toBe(3)
+    expect(stringCalculator.processInput('5,10')).toBe(15)
+    expect(stringCalculator.processInput('10,5,2')).toBe(17)
   })
 
   it('give the sum of two or more numbers delimited by comma or \\n', () => {
-    expect(calculator.processInput('1\n2')).toBe(3)
-    expect(calculator.processInput('10,5\n2')).toBe(17)
+    expect(stringCalculator.processInput('1\n2')).toBe(3)
+    expect(stringCalculator.processInput('10,5\n2')).toBe(17)
   })
 
   it('give the sum of two or more numbers delimited by an especific delimiter', () => {
-    expect(calculator.processInput('//;\n1;2')).toBe(3)
-    expect(calculator.processInput('//;\n10;5;2')).toBe(17)
-    expect(calculator.processInput('//f\n10f5f2')).toBe(17)
+    expect(stringCalculator.processInput('//;\n1;2')).toBe(3)
+    expect(stringCalculator.processInput('//;\n10;5;2')).toBe(17)
+    expect(stringCalculator.processInput('//f\n10f5f2')).toBe(17)
   })
 
   it('not accept negatives numbers', () => {
-    expect(() => calculator.processInput('-1')).toThrowError('negatives not allowed')
-    expect(() => calculator.processInput('//;\n-1;2')).toThrowError('negatives not allowed')
-    expect(() => calculator.processInput('//-\n1-2--3')).toThrowError('negatives not allowed')
-    expect(() => calculator.processInput('//-\n-1-2--3')).toThrowError('negatives not allowed')
+    expect(() => stringCalculator.processInput('-1')).toThrowError('negatives not allowed')
+    expect(() => stringCalculator.processInput('//;\n-1;2')).toThrowError('negatives not allowed')
+    expect(() => stringCalculator.processInput('//-\n1-2--3')).toThrowError('negatives not allowed')
+    expect(() => stringCalculator.processInput('//-\n-1-2--3')).toThrowError('negatives not allowed')
   })
 
   it('ignores numbers bigger than 1000', () => {
-    expect(calculator.processInput('1001')).toBe(0)
-    expect(calculator.processInput('//;\n1001;2')).toBe(2)
-    expect(calculator.processInput('1001,2,3')).toBe(5)
+    expect(stringCalculator.processInput('1001')).toBe(0)
+    expect(stringCalculator.processInput('//;\n1001;2')).toBe(2)
+    expect(stringCalculator.processInput('1001,2,3')).toBe(5)
   })
 
   it('accept delimiters specified of any length', () => {
-    expect(calculator.processInput('//[;;]\n1;;2')).toBe(3)
-    expect(calculator.processInput('//[***]\n5***1***3')).toBe(9)
-    expect(calculator.processInput('//[--]\n5--1--3')).toBe(9)
+    expect(stringCalculator.processInput('//[;;]\n1;;2')).toBe(3)
+    expect(stringCalculator.processInput('//[***]\n5***1***3')).toBe(9)
+    expect(stringCalculator.processInput('//[--]\n5--1--3')).toBe(9)
+    expect(stringCalculator.processInput('//[||]\n5||1||3')).toBe(9)
   })
 
   it('accept multiple delimiters specified', () => {
-    expect(calculator.processInput('//[;][*]\n1;1*1')).toBe(3)
-    expect(calculator.processInput('//[;][w]\n5w1;3')).toBe(9)
-    expect(calculator.processInput('//[;][-]\n5-1;3')).toBe(9)
+    expect(stringCalculator.processInput('//[;][*]\n1;1*1')).toBe(3)
+    expect(stringCalculator.processInput('//[;][w]\n5w1;3')).toBe(9)
+    expect(stringCalculator.processInput('//[;][-]\n5-1;3')).toBe(9)
   })
 
   it('accept multiple delimiters of any length specified', () => {
-    expect(calculator.processInput('//[;;][*]\n1;;1*1')).toBe(3)
-    expect(calculator.processInput('//[;;][ww]\n5ww1;;3')).toBe(9)
-    expect(calculator.processInput('//[;;][--]\n5--1;;3')).toBe(9)
+    expect(stringCalculator.processInput('//[;;][*]\n1;;1*1')).toBe(3)
+    expect(stringCalculator.processInput('//[;;][ww]\n5ww1;;3')).toBe(9)
+    expect(stringCalculator.processInput('//[;;][--]\n5--1;;3')).toBe(9)
   })
 
   it('ignore letters', () => {
-    expect(calculator.processInput('//;\n1;aa;2')).toBe(3)
-    expect(calculator.processInput('//[;;][*]\n1;;qwwq;;1*ss*1')).toBe(3)
+    expect(stringCalculator.processInput('//;\n1;aa;2')).toBe(3)
+    expect(stringCalculator.processInput('//[;;][*]\n1;;qwwq;;1*ss*1')).toBe(3)
+  })
+
+  it('not accept invalid syntax', () => {
+    expect(() => stringCalculator.processInput('//[***]\n5****1***3')).toThrowError('invalid syntax')
+    expect(() => stringCalculator.processInput('//[;;][*]\n1;;;1*1')).toThrowError('invalid syntax')
   })
 })
